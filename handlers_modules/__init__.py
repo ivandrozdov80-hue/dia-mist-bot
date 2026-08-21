dfb05782755ea65f6a73c4997ae8595248fa0f4a
ivandrozdov80-hue <ivandrozdov80@gmail.com>
2026-08-21 22:03:42 +0500

    4.1

--- handlers_modules/__init__.py
+++ handlers_modules/__init__.py
@@ -2,12 +2,16 @@
 from .admin import (
     handle_admin_newvisit,
     handle_admin_command,
+    handle_admin_create_raffle,
+    handle_admin_draw,
+    handle_status,
+    handle_stat,
+    handle_delete_guest,
     delete_guest,
     list_guests,
     clear_cache,
     restore_guests
 )
-
 from .profile import handle_profile
 from .registration import (
     get_agreement_keyboard,
@@ -16,11 +20,16 @@ from .registration import (
     PHONE_REQUEST_MESSAGES,
     handle_registration_step
 )
-
-from .visits import handle_visit
+from .visits import (
+    handle_visit_button,
+    handle_visit_request,
+    handle_visit_manual,
+    handle_admin_confirm,
+    handle_admin_reject
+)
 from .promo import handle_promo
-from .raffle import handle_raffle
-from .reviews import handle_reviews
-from .greetings import handle_greetings
+from .raffle import handle_raffle_info, handle_raffle_participate
+from .reviews import handle_review_response
+from .greetings import handle_greeting, handle_emoji_short, handle_random_joke, handle_sticker
 from .help import handle_help
-from .utils import handle_utils
+from .utils import update_command_count, update_raffle_participation
