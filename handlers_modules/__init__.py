4b700079fd986b7b2f6a370738c60c7730b45a9b
ivandrozdov80-hue <ivandrozdov80@gmail.com>
2026-08-21 20:11:41 +0500

    3.3

--- handlers_modules/__init__.py
+++ handlers_modules/__init__.py
@@ -1,4 +1,8 @@
 # handlers_modules/__init__.py
+"""
+Инициализация пакета handlers_modules.
+Экспортирует все основные обработчики для использования в других частях бота.
+"""
 from .registration import handle_new_guest, handle_registration_step, ensure_agreement
 from .profile import handle_profile
 from .visits import (
@@ -9,9 +13,16 @@ from .visits import (
     handle_admin_reject
 )
 from .raffle import handle_raffle_info, handle_raffle_participate
-from .admin import handle_admin_newvisit, handle_admin_create_raffle, handle_admin_draw, handle_status, handle_stat, handle_delete_guest
+from .admin import (
+    handle_admin_newvisit,
+    handle_admin_create_raffle,
+    handle_admin_draw,
+    handle_status,
+    handle_stat,
+    handle_delete_guest
+)
 from .greetings import handle_greeting, handle_emoji_short, handle_random_joke, handle_sticker
 from .promo import handle_promo
 from .help import handle_help
-from .utils import update_command_count, update_message_count, update_raffle_participation
+from .utils import update_command_count, update_raffle_participation
 from .reviews import handle_review_response, ask_review
